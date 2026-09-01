4ebed5a921ebd5b090e4306f01843799d5377e8c
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2026-09-02 00:08:25 +0200

    Fix Express SPA fallback route for dist static serving

--- server/index.js
+++ server/index.js
@@ -40,7 +40,7 @@ app.get('/api/health', (req, res) => {
 const distPath = path.join(__dirname, '../dist');
 if (fs.existsSync(distPath)) {
   app.use(express.static(distPath));
-  app.get('*', (req, res) => {
+  app.use((req, res) => {
     res.sendFile(path.join(distPath, 'index.html'));
   });
 }
